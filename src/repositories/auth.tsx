2938939cebc37254cbf66a66c80f83685d290515
masakura-hwcreate <masakura@hw-create.com>
2025-04-30 23:06:57 +0900

    create_auth.tsx

---
+++ src/repositories/auth.tsx
@@ -0,0 +1,27 @@
+import { supabase } from "../lib/supabase";
+
+type SignupResult = {
+    id: string;
+    email: string;
+    userName: string;
+};
+
+export const authRepository = {
+    async signup(name: string, email: string, password: string): Promise<SignupResult> {
+        const {data, error} = await supabase.auth.signUp ({
+            email,
+            password,
+            options: {data: { name }},
+        });
+
+        if(error !== null) throw new Error(error.message);
+        if (!data.user) { throw new Error("User data is missing"); }
+
+        return {
+            id: data.user.id,
+            email: data.user.email ?? "",
+            userName: data.user.user_metadata.name
+        }
+        // return { ...data.user, userName: data.user.user_metadata.name };
+    }
+}
